44c9f4792ab6170f7ad2e343b86fce0a8dde70d1
arbaz-17 <126326894+arbaz-17@users.noreply.github.com>
2026-07-24 12:18:38 +0500

    Added Subscribe and Unsubscribe and Validations in Event Manager

---
+++ src/raceControl/createRaceControl.js
@@ -0,0 +1,60 @@
+export function createRaceControlEventManager() {
+  const eventRegistry = new Map();
+
+  function validateEvent(event) {
+    if (typeof event !== "string" || event.trim() === "") {
+      throw new TypeError("Event should be a non-empty string.");
+    }
+  }
+
+  function validateHandler(handler) {
+    if (typeof handler !== "function") {
+      throw new TypeError("Handler must always be a function.");
+    }
+  }
+
+  function validatePayload(payload) {
+    if ( payload !== undefined && (payload === null || typeof payload !== "object")) {
+      throw new TypeError("Payload should be a non-empty object.");
+    }
+  }
+
+  function subscribe(event, handler) {
+    validateEvent(event);
+    validateHandler(handler);
+
+    let eventHandlers = eventRegistry.get(event);
+
+    if (!eventHandlers) {
+      eventHandlers = new Set();
+      eventRegistry.set(event, eventHandlers);
+    }
+
+    eventHandlers.add(handler);
+
+    return function cleanupSub() {
+      return unsubscribe(event, handler);
+    };
+  }
+
+  function unsubscribe(event, handler) {
+    validateEvent(event);
+    validateHandler(handler);
+
+    let eventHandlers = eventRegistry.get(event);
+
+    if (!eventHandlers) {
+      return false;
+    }
+
+    const handlerRemoved = eventHandlers.delete(handler);
+
+    if (!eventHandlers.size === 0) {
+      eventRegistry.delete(event);
+    }
+
+    return handlerRemoved;
+  }
+
+  function emit(event, payload) {}
+}
